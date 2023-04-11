a7e5ec7bc94f0c1858634bbe6d253a1dc449988d
xxmarcola <96895964+xxmarcola@users.noreply.github.com>
2023-04-10 23:14:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("form-atividade")
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const inputNomeAtividade = Document.getElementById("nome-atividade")
+    const inputNotaAtividade = Document.getElementById("nota-atividade")
+})
